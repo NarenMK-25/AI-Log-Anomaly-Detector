1e5c7f0c350642094719a4f4d95b8295a23be38e
sharu-khan-pixel <sharukhan.f.k.2007@gmail.com>
2026-05-05 10:21:18 +0530

    Update log_generator.py

--- log_generator.py
+++ log_generator.py
@@ -1,1 +1,21 @@
+import random
+import time
 
+log_file = "logs.txt"
+
+while True:
+    # 25% chance of generating an ERROR, 75% chance of INFO
+    status = random.choice(["INFO", "INFO", "INFO", "ERROR"])
+    msg = random.choice([
+        "User login",
+        "Request processed",
+        "Database query",
+        "Timeout error",
+        "Server overload"
+    ])
+
+    with open(log_file, "a") as f:
+        f.write(f"{status}: {msg}\n")
+
+    print(status, msg)
+    time.sleep(2)
